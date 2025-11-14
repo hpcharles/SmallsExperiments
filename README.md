c27431a8440c3a6bed25cf41cffa1d0418406e75
Henri-Pierre CHARLES <Henri-Pierre.Charles@cea.fr>
2025-11-14 16:46:37 +0100

    Ajout time + README

--- README.md
+++ README.md
@@ -2,10 +2,19 @@
 
 This repository provide small codes which helps to experiment with compilers
 
+## SizeOfType
+
 * SizeOfTypes.c will print the differrent size of basic C data type
+
+## VectorAdd
+
+Basic question, is there a difference between vector addition, if the
+vector is statically defined or dynamically defined ? 
+
 * VectorAdd.c is a vector addition with 2 implementations :
   * Static : loop and vector are statically defined
   * Dynamic : loop and vector are dyanmically defined (via the command line)
+
 
 ## References:
 
--- VectorAdd.c
+++ VectorAdd.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <time.h>
 #include "cycle.h"
 // riscv32-linux-gnu-gcc -S VectorAdd.c -O3  -fopt-info
 // aarch64-linux-gnu-gcc -S VectorAdd.c -O3  -fopt-info
 
 // Static definitions to help compiler
-#define VSIZE 10000
+#define VSIZE 100000000
 typedef  int vector_t[VSIZE];
 vector_t sx, sy, sz;
 
 // Function VaddStatic should be
 // * unrolled (depending on VSIZE)
-// * vectorized (depending on targer processor & compiler & optimisation option)
+// * vectorized (depending on target processor & compiler & optimisation option)
 void VaddStatic(vector_t a, vector_t b, vector_t r)
 {
   for (int i = 0; i< VSIZE; i++)
@@ -21,7 +22,7 @@ void VaddStatic(vector_t a, vector_t b, vector_t r)
 
 // Function VaddDynamic should be
 // * unrolled 
-// * vectorized (depending on targer processor & compiler & optimisation option)
+// * vectorized (depending on target processor & compiler & optimisation option)
 void VaddDynamic(vector_t a, vector_t b, vector_t r, int vSize)
 {
   for (int i = 0; i< vSize; i++)
@@ -52,6 +53,7 @@ int main(int argc, char * argv[])
 {
   int vSize;
   uint64_t ticks;
+  clock_t temps;
 
   if (argc < 2)
     {
@@ -59,16 +61,17 @@ int main(int argc, char * argv[])
       exit(-1);
     }
 
-  // Static data set
-  FillVector(sx,     0,  1, VSIZE);
+  FillVector(sx,     0,  1, VSIZE);   // Static data set
   FillVector(sy, VSIZE, -1, VSIZE);
   ticks = getticks();
+  temps = clock();
   VaddStatic(sx, sy, sz);
   ticks = getticks() - ticks;
-  printf ("        %10s %10s\n", "size" , "ticks" );
-  printf ("Static  %10d %10ld \n", VSIZE, ticks);
+  temps = clock() - temps;
+  printf ("        %10s %10s %10s %10s\n", "size" , "ticks", "time (s)", "dummy value");
+  printf ("Static  %10d %10ld %10f %10d\n", VSIZE, ticks, ((double) temps)/CLOCKS_PER_SEC, sz[0]);
+  // print sz[0] to avoid aggressive optimization
   //  printVector ("Résultat Statique", sz, VSIZE, 10);
-
 
   int * dx, *dy, *dz;   // Dynamic data set
   vSize = atoi(argv[1]);
@@ -78,9 +81,12 @@ int main(int argc, char * argv[])
   FillVector(dx,     0,  1, vSize);
   FillVector(dy, vSize, -1, vSize);
   ticks = getticks();
+  temps = clock();
   VaddDynamic(dx, dy, dz, vSize);
   ticks = getticks() - ticks;
-  printf ("Dynamic %10d %10ld\n", vSize, ticks);
+  temps = clock() - temps;
+  printf ("Dynamic %10d %10ld %10f %10d\n", vSize, ticks, ((double) temps)/CLOCKS_PER_SEC, sz[0]);
+  // print dz[0] to avoid aggressive optimization
   //  printVector ("Résultat Statique", dz, vSize, 10);
   return 0;
 }
